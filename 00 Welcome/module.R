7eaa126c52f977a4bdc7c56a22d165627de2f29c
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2019-03-13 10:53:35 +0100

    Small changes in Welcome

--- 00 Welcome/module.R
+++ 00 Welcome/module.R
@@ -13,11 +13,10 @@ Wel_ui <- function(id) {
     ns <- NS(id)
     list(tabItem(tabName = str_c(id, "Wel1"),
         fluidPage(
-            column(12, align = "Center", h2(HTML("<b> Welcome to DISNET dashboard!! </b>")),
-            p(HTML("<b> Daniel Minguez Camacho </b>")),
-            p(HTML("<b> Javier de la Rua Martinez </b>")),
-            p(HTML("<b> Filip Finfando </b>")))
-            )))
+            column(12, align = "Center", h2(style = "font-size: 4em; margin: 60px 0 80px;", HTML("<b>Welcome to DISNET dashboard</b>"))),
+            column(4, align = "Center", p(style = "font-size: 1.4em", "Daniel Minguez Camacho")),
+            column(4, align = "Center", p(style = "font-size: 1.4em", "Javier de la Rua Martinez")),
+            column(4, align = "Center", p(style = "font-size: 1.4em", "Filip Finfando")))))
 }
 
 # Server
