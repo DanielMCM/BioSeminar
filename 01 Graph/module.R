bd9fcf61a4f13aacd98ac9bfb41d05f75fe05ba7
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2019-03-15 11:25:10 +0100

    Reorder layout

--- 01 Graph/module.R
+++ 01 Graph/module.R
@@ -15,16 +15,15 @@ Gra_menuItem <- function(id) {
 Gra_ui <- function(id) {
     ns <- NS(id)
     list(tabItem(tabName = str_c(id, "Graph"),
-        fluidRow(box("The idea is as follows: 
+        fluidRow(box(width = 6, "The idea is as follows: 
                         (i) instert diseases
                         (ii) click generate connections (each time you click you add adjacent nodes) 
                         (iii) if you click in add symptoms (then on generate 1 level), you will aggregate the symptoms to each non-DISNET node
-                        (iv) after this step you should restart the app to start again (or you will obtain wrong results")), 
-        fluidRow(box("ERRORS DETECTED:
+                        (iv) after this step you should restart the app to start again (or you will obtain wrong results"), 
+                box(width = 6, "ERRORS DETECTED:
                         (i) Some nodes produces error when calling to the API (Not responsive for some codes)
                         (ii) if there is no response from API or there are no CUIs associated to a node, nothing is added")),
-
-        box(width = 3,
+        fluidRow(box(width = 3,
             h3("Query options"),
             selectizeInput(ns("select"), "Select any node", choices = NULL, options = NULL, multiple = TRUE),
             actionButton(ns("do_link"), "Add symptoms"),
@@ -34,7 +33,7 @@ Gra_ui <- function(id) {
             verbatimTextOutput(ns("NNodes"))),
         box(width = 9, align = "Center",
             h2("Diseases graph"), p(verbatimTextOutput(ns("message"))),
-            visNetworkOutput(ns("network")))))
+            visNetworkOutput(ns("network"))))))
 }
 
 # Server
